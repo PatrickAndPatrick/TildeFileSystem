8263dbc90526a616dc92dae7dfa02b2c98d67e16
patrickAbbs <patricka@headspring.com>
2013-03-24 17:34:15 -0500

    set up basic code structure

---
+++ tilde/tilde/Controllers/FolderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tilde.Models;
+
+namespace tilde.Controllers
+{
+    public class FolderController : Controller
+    {
+        //
+        // GET: /Folder/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View("");
+        }
+
+        [HttpPost]
+        public ActionResult Upload(string directoryPath)
+        {
+
+            //perform file upload
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DisplayFolderList()
+        {
+
+            //get folders
+
+            return View(new List<FolderModel>());
+        }
+
+        public ActionResult DisplayFilesFromFolder(Guid folderId)
+        {
+            return PartialView(new FolderModel());
+        }
+
+        public ActionResult PopOutFileText(Guid fileId)
+        {
+            return PartialView(new FileModel());
+        }
+
+    }
+}
--- tilde/tilde/Global.asax.cs
+++ tilde/tilde/Global.asax.cs
@@ -24,7 +24,7 @@ namespace tilde
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Folder", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
